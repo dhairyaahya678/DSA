d4b932d9e6dfd50e9301e14a086b20bdcfaa960e
Dhairya Ahya <109900205+dhairyaahya678@users.noreply.github.com>
2024-07-17 12:32:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse array in groups/reverse-array-in-groups.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+
+    // Function to reverse every sub-array group of size k.
+    void reverseInGroups(ArrayList<Long> arr, int k) {
+        
+        for(int i = 0;i<arr.size();i+=k){
+            int end = Math.min(i+k,arr.size());
+            Collections.reverse(arr.subList(i,end));
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // consume the newline
+        while (t-- > 0) {
+            int k = sc.nextInt();
+            sc.nextLine(); // consume the newline
+            String input = sc.nextLine();
+            ArrayList<Long> arr = new ArrayList<>();
+            Scanner ss = new Scanner(input);
+            while (ss.hasNextLong()) {
+                arr.add(ss.nextLong());
+            }
+            Solution ob = new Solution();
+            ob.reverseInGroups(arr, k);
+            for (long num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
