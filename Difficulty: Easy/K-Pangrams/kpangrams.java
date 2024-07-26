70b0069892767a241c89b673b82fe362c3c3d436
Dhairya Ahya <109900205+dhairyaahya678@users.noreply.github.com>
2024-07-26 23:23:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-Pangrams/kpangrams.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine().trim());
+        while (t-- > 0) {
+            String str = sc.nextLine();
+            int k = Integer.parseInt(sc.nextLine().trim());
+            Solution obj = new Solution();
+            if (obj.kPangram(str, k))
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    boolean kPangram(String str, int k) {
+        // code here
+        String str1 = str.replaceAll("\\s", "");
+        HashSet<Character>set=new HashSet<>();
+        for(int i=0;i<str1.length();i++){
+            char ch=str1.charAt(i);
+            set.add(ch);
+        }
+        if(str1.length()>=26&&set.size()+k>=26){
+            return true;
+        }
+        return false;
+    }
+}
+
