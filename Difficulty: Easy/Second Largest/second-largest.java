d9275d6849b418339265983f0bda67bc127ed0d8
Dhairya Ahya <109900205+dhairyaahya678@users.noreply.github.com>
2024-11-15 15:05:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -10,15 +10,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
         while (t-- > 0) {
-            List<Integer> arr = new ArrayList<>();
-            String input = sc.nextLine();
-            Scanner ss = new Scanner(input);
-            while (ss.hasNextInt()) {
-                arr.add(ss.nextInt());
-            }
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
             Solution ob = new Solution();
-            int ans = ob.print2largest(arr);
+            int ans = ob.getSecondLargest(arr);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -29,19 +27,20 @@ public class Main {
 // User function Template for Java
 
 class Solution {
-    public int print2largest(List<Integer> arr) {
+    public int getSecondLargest(int[] arr) {
         // Code Here
-        int max = Integer.MIN_VALUE;
-        int smax = Integer.MIN_VALUE;
-        for(int i:arr){
-            if(i>max){
-                smax=max;
-                max=i;
-            }
-            else if(i<max && i>smax){
-                smax=i;
+        int max=0;
+        int sec = -1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
             }
         }
-        return smax;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>sec && arr[i]<max){
+                sec=arr[i];
+            }
+        }
+        return sec;
     }
 }
